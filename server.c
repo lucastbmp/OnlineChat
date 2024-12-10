a5e289c0c6861a22ae37167a80f46cf936b590c7
nicollasprado <nicollasprado.profissional@gmail.com>
2024-12-09 23:12:53 -0300

    Inicio da configuração do servidor socket

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <sys/socket.h>
+#include <stdio.h>
+
+int main(){
+    // Return a file descriptor
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+
+
+    // Connect needs a file descriptor
+    connect
+    return 0;
+}
